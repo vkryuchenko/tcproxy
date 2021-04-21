a9a0464a996bb1e295d8b633a0122aca50ffc4a9
Vyacheslav Kryuchenko <newnis.vrn@gmail.com>
2021-04-21 12:09:22 +0300

    internal: update

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ export GOPATH=$GOPATH:`pwd`
 export GOARCH=amd64
 
 export GOOS=linux
-go build -ldflags "-w -s -linkmode internal" -o tcproxy-linux src/tcproxy.go
+go build -ldflags "-w -s -linkmode internal" -o tcproxy-linux tcproxy.go
 export GOOS=darwin
-go build -ldflags "-w -s -linkmode internal" -o tcproxy-mac src/tcproxy.go
+go build -ldflags "-w -s -linkmode internal" -o tcproxy-mac tcproxy.go
 export GOOS=windows
-go build -ldflags "-w -s -linkmode internal" -o tcproxy-win.exe src/tcproxy.go
+go build -ldflags "-w -s -linkmode internal" -o tcproxy-win.exe tcproxy.go
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module tcproxy
 
-go 1.12
+go 1.16
